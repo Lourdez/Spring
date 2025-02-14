6139646158402055f9d052b3fe41e40dd42c7d5b
parker <54675124+Lourdez@users.noreply.github.com>
2025-02-14 12:37:54 +0530

    first commit

---
+++ src/main/java/com/hello/helloworld/HelloworldApplication.java
@@ -0,0 +1,36 @@
+package com.hello.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class HelloworldApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloworldApplication.class);
+    }
+}
+
+
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class HelloworldApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(HelloworldApplication.class, args);
+//	}
+//
+//}
+// Default Spring Application
+//
+
