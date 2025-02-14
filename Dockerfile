ccb94151484eab69afe00175e9c92e68c60ddf2b
parker <54675124+Lourdez@users.noreply.github.com>
2025-02-15 00:07:16 +0530

    updated

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM tomcat:latest
+# Take the war and copy to webapps of tomcat
+COPY target/newapp.war /usr/local/tomcat/webapps/
--- src/main/java/com/hello/helloworld/HelloworldApplication.java
+++ src/main/java/com/hello/helloworld/HelloworldApplication.java
@@ -20,6 +20,7 @@ public class HelloworldApplication extends SpringBootServletInitializer {
 
 
 
+
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
